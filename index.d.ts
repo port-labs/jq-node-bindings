dbd13b07f40899c7e8f7cc5b06e79fdb1148eb3a
talsabagport <tal@getport.io>
2024-01-22 15:12:49 +0200

    Add support to throw jq error

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module '@port-labs/jq-node-bindings' {
-    export function exec(json: object, input: string, options?: {enableEnv?: boolean}): object | Array<any> | string | number | boolean | null;
+    export function exec(json: object, input: string, options?: {enableEnv?: boolean, throwOnError?: boolean}): object | Array<any> | string | number | boolean | null;
     export function renderRecursively(json: object, input: object | Array<any> | string | number | boolean | null): object | Array<any> | string | number | boolean | null;
 }
--- src/binding.cc
+++ src/binding.cc
@@ -56,6 +56,20 @@ void jv_object_to_v8(std::string key, jv actual, v8::Local<v8::Object> ret) {
     v8::Local<v8::Value> v8_val;
 
     switch (k) {
+      case JV_KIND_INVALID: {
+          jv msg = jv_invalid_get_msg(jv_copy(actual));
+          char err[4096];
+          if (jv_get_kind(msg) == JV_KIND_STRING) {
+            snprintf(err, sizeof(err), "jq: error: %s", jv_string_value(msg));
+            jv_free(msg);
+            jv_free(actual);
+            Nan::ThrowTypeError(err);
+            return;
+          }
+          jv_free(msg);
+          jv_free(actual);
+          break;
+      }
       case JV_KIND_NULL: {
           v8_val = Nan::Null();
           break;
@@ -104,25 +118,35 @@ void jv_object_to_v8(std::string key, jv actual, v8::Local<v8::Object> ret) {
     Nan::Set(ret, v8_key, v8_val);
 }
 
+struct err_data {
+  char buf[4096];
+};
+
+void throw_err_cb(void *data, jv msg) {
+  struct err_data *err_data = (struct err_data *)data;
+  if (jv_get_kind(msg) != JV_KIND_STRING)
+    msg = jv_dump_string(msg, JV_PRINT_INVALID);
+  if (!strncmp(jv_string_value(msg), "jq: error", sizeof("jq: error") - 1))
+    snprintf(err_data->buf, sizeof(err_data->buf), "%s", jv_string_value(msg));
+  if (strchr(err_data->buf, '\n'))
+    *(strchr(err_data->buf, '\n')) = '\0';
+  jv_free(msg);
+}
+
 void jq_exec(std::string json, std::string filter,const Nan::FunctionCallbackInfo<v8::Value>& info) {
     jq_state *jq = NULL;
+    struct err_data err_msg;
 
     if (cache.exist(filter)) {
         jq = cache.get(filter);
     } else {
         jq = jq_init();
+        jq_set_error_cb(jq, throw_err_cb, &err_msg);
         if (!jq_compile(jq, filter.c_str())) {
-            info.GetReturnValue().Set(Nan::Null());
+            Nan::ThrowTypeError(err_msg.buf);
             return;
         }
         cache.put(filter, jq);
-    }
-
-    jv input = jv_parse(json.c_str());
-
-    if (!jv_is_valid(input)) {
-        info.GetReturnValue().Set(Nan::Null());
-        return;
     }
 
     if (jq == NULL) {
@@ -130,14 +154,19 @@ void jq_exec(std::string json, std::string filter,const Nan::FunctionCallbackInf
         return;
     }
 
-    jq_start(jq, input, 0);
+    jv input = jv_parse(json.c_str());
 
-    jv actual = jq_next(jq);
-    jv_kind k = jv_get_kind(actual);
+    if (!jv_is_valid(input)) {
+        info.GetReturnValue().Set(Nan::Null());
+        jv_free(input);
+        return;
+    }
+
+    jq_start(jq, input, 0);
+    jv result = jq_next(jq);
 
     v8::Local<v8::Object> ret = Nan::New<v8::Object>();
-
-    jv_object_to_v8("value", actual, ret);
+    jv_object_to_v8("value", result, ret);
 
     info.GetReturnValue().Set(ret);
 }
@@ -149,14 +178,14 @@ std::string FromV8String(v8::Local<v8::String> val) {
 }
 
 void Exec(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-        v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
-
         if (info.Length() < 2) {
             Nan::ThrowTypeError("Wrong number of arguments");
+            return;
         }
 
         if (!info[0]->IsString() || !info[1]->IsString()) {
             Nan::ThrowTypeError("Wrong arguments");
+            return;
         }
 
         std::string json = FromV8String(Nan::To<v8::String>(info[0]).ToLocalChecked());
@@ -166,12 +195,10 @@ void Exec(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 }
 
 void Init(v8::Local<v8::Object> exports) {
-  v8::Local<v8::Context> context = exports->CreationContext();
-  exports->Set(context,
-               Nan::New("exec").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(Exec)
-                   ->GetFunction(context)
-                   .ToLocalChecked());
+  v8::Local<v8::Context> context = exports->GetCreationContext().ToLocalChecked();
+  (void)exports->Set(context,
+                     Nan::New("exec").ToLocalChecked(),
+                     Nan::New<v8::FunctionTemplate>(Exec)->GetFunction(context).ToLocalChecked());
 }
 
 NODE_MODULE(exec, Init)
