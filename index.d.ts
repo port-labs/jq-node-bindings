68d9bbf613a133f373c2f6932ef08e65d8e44935
talsabagport <tal@getport.io>
2024-01-22 15:33:22 +0200

    fix lint

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 declare module '@port-labs/jq-node-bindings' {
-    type ExecOptions = {enableEnv?: boolean, throwOnError?: boolean};
-    export function exec(json: object, input: string, options?: ExecOptions): object | Array<any> | string | number | boolean | null;
-    export function renderRecursively(json: object, input: object | Array<any> | string | number | boolean | null, execOptions?: ExecOptions): object | Array<any> | string | number | boolean | null;
+  type ExecOptions = { enableEnv?: boolean, throwOnError?: boolean };
+
+  export function exec(json: object, input: string, options?: ExecOptions): object | Array<any> | string | number | boolean | null;
+
+  export function renderRecursively(json: object, input: object | Array<any> | string | number | boolean | null, execOptions?: ExecOptions): object | Array<any> | string | number | boolean | null;
 }
