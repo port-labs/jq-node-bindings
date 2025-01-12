b583801e03bed2c50c60cc4b339ec5908f0b7ce3
Ivan Kalinovski <ivankalinovski@Ivans-MacBook-Pro.local>
2025-01-12 15:24:12 +0200

    test jq async

--- binding.gyp
+++ binding.gyp
@@ -3,47 +3,67 @@
         {
             "target_name": "jq-node-bindings",
             "sources": [
-                "src/binding.cc",
+                "src/binding.cc"
             ],
             "include_dirs": [
-                "<!(node -e \"require('nan')\")",
+                "<!(node -p \"require('node-addon-api').include\")",
                 "<(module_root_dir)/",
+                "node_modules/node-addon-api",
                 "deps/jq/src"
             ],
-            'conditions': [
+            "defines": [
+                "NAPI_VERSION=9"
+            ],
+            "conditions": [
                 [
-                    'OS=="linux"',
+                    "OS=='linux'",
                     {
                         "libraries": [
                             "-Wl,-rpath='$$ORIGIN/../deps'",
-                            "../build/deps/libjq.so.1",
+                            "../build/deps/libjq.so.1"
                         ],
-                        'cflags_cc': [
-                            '-std=c++17'
+                        "cflags_cc": [
+                            "-std=c++17"
                         ],
-                        'cflags_cc!': [
-                            '-fno-rtti -fno-exceptions'
+                        "cflags_cc!": [
+                            "-fno-rtti -fno-exceptions"
                         ]
                     }
                 ],
                 [
-                    'OS=="mac"',
+                    "OS=='mac'",
                     {
                         "libraries": [
-                            "../build/deps/libjq.dylib",
-                            # "../build/deps/libonig.4.dylib",
-                            # "../build/deps/libonig.dylib",
+                            "../build/deps/libjq.dylib"
                         ],
-                        'xcode_settings': {
-                            'MACOSX_DEPLOYMENT_TARGET': '12.0.1',
-                            'GCC_ENABLE_CPP_RTTI': 'YES',
-                            'GCC_ENABLE_CPP_EXCEPTIONS': 'YES'
+                        "xcode_settings": {
+                            "MACOSX_DEPLOYMENT_TARGET": "12.0.1",
+                            "GCC_ENABLE_CPP_RTTI": "YES",
+                            "GCC_ENABLE_CPP_EXCEPTIONS": "YES"
                         },
-                        'OTHER_CPLUSPLUSFLAGS': [
-                            '-std=c++17'
+                        "OTHER_CPLUSPLUSFLAGS": [
+                            "-std=c++17"
                         ],
                         "include_dirs": [
                             "deps/jq/src"
+                        ]
+                    }
+                ],
+                [
+                    "OS=='win'",
+                    {
+                        "msvs_settings": {
+                            "VCCLCompilerTool": {
+                                "AdditionalOptions": ["/std:c++17"],
+                                "ExceptionHandling": 1,
+                                "RuntimeTypeInfo": "true"
+                            }
+                        },
+                        "libraries": [
+                            "deps\\jq\\build\\Release\\libjq.lib"
+                        ],
+                        "include_dirs": [
+                            "deps\\jq\\src"
                         ]
                     }
                 ]
--- src/binding.cc
+++ src/binding.cc
@@ -1,204 +1,315 @@
-#include "src/binding.h"
 #include <list>
 #include <unordered_map>
 #include <assert.h>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include "src/binding.h"
 
-using namespace std;
 
-// LRUCache to save filter to the compiled jq_state
-template <class KEY_T, class VAL_T> class LRUCache{
+struct err_data {
+    char buf[4096]; 
+};
+void throw_err_cb(void* data, jv msg) {
+    struct err_data* err_data = static_cast<struct err_data*>(data);
+
+    if (jv_get_kind(msg) != JV_KIND_STRING) {
+        msg = jv_dump_string(msg, JV_PRINT_INVALID);
+    }
+
+    snprintf(err_data->buf, sizeof(err_data->buf), "aa%s", jv_string_value(msg));
+    jv_free(msg);
+}
+
+template <class KEY_T, class VAL_T> class LRUCache {
 private:
-        list< pair<KEY_T,VAL_T> > item_list;
-        unordered_map<KEY_T, decltype(item_list.begin()) > item_map;
-        size_t cache_size;
-private:
-        void clean(void){
-                while(item_map.size()>cache_size){
-                        auto last_it = item_list.end(); last_it --;
-                        item_map.erase(last_it->first);
-                        item_list.pop_back();
-                        jq_teardown(&last_it->second);
-                }
-        };
+    std::list<std::pair<KEY_T, VAL_T>> item_list;
+    std::unordered_map<KEY_T, decltype(item_list.begin())> item_map;
+    size_t cache_size;
+
+    void clean() {
+        while (item_map.size() > cache_size) {
+            auto last_it = item_list.end();
+            last_it--;
+            item_map.erase(last_it->first);
+            item_list.pop_back();
+            jq_teardown(&last_it->second);
+        }
+    }
+
 public:
-        LRUCache(int cache_size_):cache_size(cache_size_){
-                ;
-        };
-
-        void put(const KEY_T &key, const VAL_T &val){
-                auto it = item_map.find(key);
-                if(it != item_map.end()){
-                        item_list.erase(it->second);
-                        item_map.erase(it);
-                }
-                item_list.push_front(make_pair(key,val));
-                item_map.insert(make_pair(key, item_list.begin()));
-                clean();
-        };
-        bool exist(const KEY_T &key){
-                return (item_map.count(key)>0);
-        };
-        VAL_T get(const KEY_T &key){
-                assert(exist(key));
-                auto it = item_map.find(key);
-                item_list.splice(item_list.begin(), item_list, it->second);
-                return it->second->second;
-        };
-
+    LRUCache(int cache_size_) : cache_size(cache_size_) {}
+    void put(const KEY_T &key, const VAL_T &val) {
+        auto it = item_map.find(key);
+        if (it != item_map.end()) {
+            item_list.erase(it->second);
+            item_map.erase(it);
+        }
+        item_list.push_front(std::make_pair(key, val));
+        item_map.insert(std::make_pair(key, item_list.begin()));
+        clean();
+    }
+    bool exist(const KEY_T &key) {
+        return (item_map.count(key) > 0);
+    }
+    VAL_T get(const KEY_T &key) {
+        assert(exist(key));
+        auto it = item_map.find(key);
+        item_list.splice(item_list.begin(), item_list, it->second);
+        return it->second->second;
+    }
 };
 
 LRUCache<std::string, jq_state*> cache(100);
 
-void jv_object_to_v8(std::string key, jv actual, v8::Local<v8::Object> ret) {
-    jv_kind k = jv_get_kind(actual);
+std::string FromNapiString(napi_env env, napi_value value) {
+    size_t str_size;
+    size_t str_size_out;
+    napi_get_value_string_utf8(env, value, nullptr, 0, &str_size);
+    char* str = new char[str_size + 1];
+    napi_get_value_string_utf8(env, value, str, str_size + 1, &str_size_out);
+    std::string result(str);
+    delete[] str;
+    return result;
+}
 
-    v8::Local<v8::String> v8_key = Nan::New(key).ToLocalChecked();
-    v8::Local<v8::Value> v8_val;
+void jv_object_to_napi(std::string key, napi_env env, jv actual, napi_value ret) {
+    jv_kind kind = jv_get_kind(actual);
+    napi_value value;
 
-    switch (k) {
-      case JV_KIND_INVALID: {
-          jv msg = jv_invalid_get_msg(jv_copy(actual));
-          char err[4096];
-          if (jv_get_kind(msg) == JV_KIND_STRING) {
-            snprintf(err, sizeof(err), "jq: error: %s", jv_string_value(msg));
+    switch (kind) {
+        case JV_KIND_INVALID: {
+            jv msg = jv_invalid_get_msg(jv_copy(actual));
+            if (jv_get_kind(msg) == JV_KIND_STRING) {
+                napi_throw_error(env, nullptr, jv_string_value(msg));
+            }
             jv_free(msg);
-            jv_free(actual);
-            Nan::ThrowError(err);
-            return;
-          }
-          jv_free(msg);
-          jv_free(actual);
-          break;
-      }
-      case JV_KIND_NULL: {
-          v8_val = Nan::Null();
-          break;
-      }
-      case JV_KIND_TRUE: {
-          v8_val = Nan::True();
-          break;
-      }
-      case JV_KIND_FALSE: {
-          v8_val = Nan::False();
-          break;
-      }
-      case JV_KIND_NUMBER: {
-          v8_val = Nan::New(jv_number_value(actual));
-          break;
-      }
-      case JV_KIND_STRING: {
-          v8_val = Nan::New(jv_string_value(actual)).ToLocalChecked();
-          jv_free(actual);
-          break;
-      }
-      case JV_KIND_ARRAY: {
-          v8::Local<v8::Array> ret_arr = Nan::New<v8::Array>();
-          for (int i = 0; i < jv_array_length(jv_copy(actual)); i++) {
-            jv_object_to_v8(std::to_string(i), jv_array_get(jv_copy(actual), i), ret_arr);
-          }
-          Nan::Set(ret, v8_key, ret_arr);
-          jv_free(actual);
-          break;
-      }
-      case JV_KIND_OBJECT: {
-          v8::Local<v8::Object> ret_obj = Nan::New<v8::Object>();
-          jv_object_foreach(actual, itr_key, value) {
-            jv_object_to_v8(jv_string_value(itr_key), value, ret_obj);
-          }
-          jv_free(actual);
-          Nan::Set(ret, v8_key, ret_obj);
-          break;
-      }
+            break;
+        }
+        case JV_KIND_NULL: {
+            napi_get_null(env, &value);
+            break;
+        }
+        case JV_KIND_TRUE: {
+            napi_get_boolean(env, true, &value);
+            break;
+        }
+        case JV_KIND_FALSE: {
+            napi_get_boolean(env, false, &value);
+            break;
+        }
+        case JV_KIND_NUMBER: {
+            double num = jv_number_value(actual);
+            napi_create_double(env, num, &value);
+            break;
+        }
+        case JV_KIND_STRING: {
+            napi_create_string_utf8(env, jv_string_value(actual), NAPI_AUTO_LENGTH, &value);
+            break;
+        }
+        case JV_KIND_ARRAY: {
+            size_t arr_len = jv_array_length(jv_copy(actual));
+            napi_create_array_with_length(env, arr_len, &value);
+
+            for (size_t i = 0; i < arr_len; i++) {
+                jv elem = jv_array_get(jv_copy(actual), i);
+                jv_object_to_napi(std::to_string(i), env, elem, value);
+                jv_free(elem); 
+            }
+            break;
+        }
+        case JV_KIND_OBJECT: {
+            napi_create_object(env, &value);
+
+            jv_object_foreach(actual, obj_key, obj_value) {
+                jv_object_to_napi(jv_string_value(obj_key), env, obj_value, value);
+                jv_free(obj_key);
+                jv_free(obj_value);
+            }
+            break;
+        }
+        default:
+            napi_throw_error(env, nullptr, "Unsupported jv type");
+            break;
     }
 
-    if (v8_val.IsEmpty()) {
-        return;
+    napi_set_named_property(env, ret, key.c_str(), value);
+}
+
+
+
+
+napi_value ExecSync(napi_env env, napi_callback_info info) {
+    size_t argc = 2;
+    napi_value args[2];
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+
+    if (argc < 2) {
+        napi_throw_type_error(env, nullptr, "Wrong number of arguments. Expected 2.");
+        return nullptr;
     }
 
-    Nan::Set(ret, v8_key, v8_val);
-}
+    std::string json = FromNapiString(env, args[0]);
+    std::string filter = FromNapiString(env, args[1]);
 
-struct err_data {
-  char buf[4096];
-};
-
-void throw_err_cb(void *data, jv msg) {
-  struct err_data *err_data = (struct err_data *)data;
-  if (jv_get_kind(msg) != JV_KIND_STRING)
-    msg = jv_dump_string(msg, JV_PRINT_INVALID);
-  if (!strncmp(jv_string_value(msg), "jq: error", sizeof("jq: error") - 1))
-    snprintf(err_data->buf, sizeof(err_data->buf), "jq: compile error%s", jv_string_value(msg) + strlen("jq: error"));
-  if (strchr(err_data->buf, '\n'))
-    *(strchr(err_data->buf, '\n')) = '\0';
-  jv_free(msg);
-}
-
-void jq_exec(std::string json, std::string filter,const Nan::FunctionCallbackInfo<v8::Value>& info) {
-    jq_state *jq = NULL;
+    jq_state* jq = nullptr;
     struct err_data err_msg;
 
-    if (cache.exist(filter)) {
-        jq = cache.get(filter);
-    } else {
+    // if (cache.exist(filter)) {
+    //     jq = jv_copy(cache.get(filter));
+    // } else {
         jq = jq_init();
         jq_set_error_cb(jq, throw_err_cb, &err_msg);
         if (!jq_compile(jq, filter.c_str())) {
-            Nan::ThrowError(err_msg.buf);
-            return;
-        }
-        cache.put(filter, jq);
-    }
+            jq_teardown(&jq);
 
-    if (jq == NULL) {
-        info.GetReturnValue().Set(Nan::Null());
-        return;
+            napi_throw_error(env, nullptr, err_msg.buf);
+            return nullptr;
+        }
+    //     cache.put(filter, jv jv_copy(jq));
+    // }
+
+    if (jq == nullptr) {
+        napi_throw_error(env, nullptr, "Failed to initialize jq");
+        jq_teardown(&jq);
+
+        return nullptr;
     }
 
     jv input = jv_parse(json.c_str());
-
     if (!jv_is_valid(input)) {
-        info.GetReturnValue().Set(Nan::Null());
         jv_free(input);
-        return;
+        napi_throw_error(env, nullptr, "Invalid JSON input");
+        jq_teardown(&jq);
+
+        return nullptr;
     }
 
     jq_start(jq, input, 0);
     jv result = jq_next(jq);
 
-    v8::Local<v8::Object> ret = Nan::New<v8::Object>();
-    jv_object_to_v8("value", result, ret);
+    napi_value ret;
+    napi_create_object(env, &ret);
 
-    info.GetReturnValue().Set(ret);
+    jv_object_to_napi("value",env,result,ret);
+    jv_free(result);
+jq_teardown(&jq);
+    return ret;
 }
 
+struct AsyncWork {
+    std::string json;
+    std::string filter;
+    napi_deferred deferred;
+    jv result;
+    std::string error;
+    bool success;
+};
 
-std::string FromV8String(v8::Local<v8::String> val) {
-    Nan::Utf8String keyUTF8(val);
-    return std::string(*keyUTF8);
-}
+void ExecuteAsync(napi_env env, void* data) {
+    AsyncWork* work = static_cast<AsyncWork*>(data);
+    jq_state* jq = nullptr;
+    struct err_data err_msg;
 
-void Exec(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-        if (info.Length() < 2) {
-            Nan::ThrowTypeError("Wrong number of arguments");
+    // if (cache.exist(work->filter)) {
+    //     jq = &jv_copy(cache.get(work->filter));
+    // } else {
+        jq = jq_init();
+        jq_set_error_cb(jq, throw_err_cb, &err_msg);
+        if (!jq_compile(jq, work->filter.c_str())) {
+            work->error = err_msg.buf;
+            work->success = false;
+jq_teardown(&jq);
+
             return;
         }
+    //     cache.put(work->filter, jv_copy(jq));
+    // }
 
-        if (!info[0]->IsString() || !info[1]->IsString()) {
-            Nan::ThrowTypeError("Wrong arguments");
-            return;
-        }
+    jv input = jv_parse(work->json.c_str());
+    if (!jv_is_valid(input)) {
+        work->error = "Invalid JSON input";
+        work->success = false;
+jq_teardown(&jq);
+        jv_free(input);
 
-        std::string json = FromV8String(Nan::To<v8::String>(info[0]).ToLocalChecked());
-        std::string filter = FromV8String(Nan::To<v8::String>(info[1]).ToLocalChecked());
+        return;
+    }
 
-        jq_exec(json, filter, info);
+    jq_start(jq, input, 0);
+    work->result = jq_next(jq);
+
+    if (!jv_is_valid(work->result)) {
+        work->error = "jq execution failed";
+        work->success = false;
+        return;
+    }
+
+    work->success = true;
 }
 
-void Init(v8::Local<v8::Object> exports) {
-  v8::Local<v8::Context> context = exports->GetCreationContext().ToLocalChecked();
-  (void)exports->Set(context,
-                     Nan::New("exec").ToLocalChecked(),
-                     Nan::New<v8::FunctionTemplate>(Exec)->GetFunction(context).ToLocalChecked());
+
+void CompleteAsync(napi_env env, napi_status status, void* data) {
+    AsyncWork* work = static_cast<AsyncWork*>(data);
+
+    if (work->success) {
+        napi_value ret;
+        napi_create_object(env, &ret);
+
+        jv_object_to_napi("value", env, work->result, ret);
+
+        napi_resolve_deferred(env, work->deferred, ret);
+    } else {
+        napi_value error;
+        napi_create_string_utf8(env, work->error.c_str(), NAPI_AUTO_LENGTH, &error);
+        napi_reject_deferred(env, work->deferred, error);
+    }
+        jv_free(work->result);
+
+    delete work; 
 }
 
-NODE_MODULE(exec, Init)
+
+
+napi_value ExecAsync(napi_env env, napi_callback_info info) {
+    size_t argc = 2;
+    napi_value args[2];
+    napi_value promise;
+
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+
+    if (argc < 2) {
+        napi_throw_type_error(env, nullptr, "Wrong number of arguments. Expected 2.");
+        return nullptr;
+    }
+
+    AsyncWork* work = new AsyncWork();
+    work->json = FromNapiString(env, args[0]);
+    work->filter = FromNapiString(env, args[1]);
+    work->success = false;
+
+    napi_create_promise(env, &work->deferred, &promise);
+
+    napi_value resource_name;
+    napi_create_string_utf8(env, "ExecAsync", NAPI_AUTO_LENGTH, &resource_name);
+
+    napi_async_work async_work;
+    napi_create_async_work(env, nullptr, resource_name, ExecuteAsync, CompleteAsync, work, &async_work);
+    napi_queue_async_work(env, async_work);
+
+    return promise;
+}
+
+napi_value Init(napi_env env, napi_value exports) {
+    napi_value exec_sync, exec_async;
+
+    napi_create_function(env, "execSync", NAPI_AUTO_LENGTH, ExecSync, nullptr, &exec_sync);
+    napi_create_function(env, "execAsync", NAPI_AUTO_LENGTH, ExecAsync, nullptr, &exec_async);
+
+    napi_set_named_property(env, exports, "execSync", exec_sync);
+    napi_set_named_property(env, exports, "execAsync", exec_async);
+
+    return exports;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
